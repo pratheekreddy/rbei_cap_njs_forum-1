aef19c2274e919514319fe1e63f475fa7fdc8298
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-09-06 07:05:34 +0000

    added authentication to CAP services

--- rbei_njs_forum/middleware/auth.js
+++ rbei_njs_forum/middleware/auth.js
@@ -1,57 +1,57 @@
 "use strict";
 const jwt = require('jsonwebtoken');
 
-const auth = async(req, res, next) => {
+const auth = async (req, res, next) => {
 
-	try {
+    try {
 
-		let authorization = req.headers.authorization;
-		if (!authorization) {
-			return res.status(401).send({
-				msg: 'unauthorized'
-			});
-		}
-		let headers = authorization.split(';');
-		console.log(headers);
-		if (headers.length != 2 || !headers[0].startsWith('requester=') || !headers[1].startsWith('rbei_access_token=')) return res.status(401).send({
-			msg: 'unauthorized'
-		});
-		let requester = headers[0].replace('requester=', '');
-		let rbei_access_token = headers[1].replace('rbei_access_token=', '');
-		
-		//verify the token.
-		let secretKey = '$7ckugsc@#~oindjsad%9';
-		const decoded = jwt.verify(rbei_access_token, secretKey);
-		const {
-			EMAIL_ID
-		} = decoded;
-		if (!(requester === EMAIL_ID)) return res.status(401).send({
-			msg: 'unauthorized'
-		});
-		
-		
-		//will return only one record as email_id is the key
-		const query = 'SELECT EMAIL_ID, STATUS, TYPE FROM RBEI_CUSTOM_AUTH_T_MD_USER WHERE EMAIL_ID = ?';
-		const client = req.db;
-		const result = await client.exec(query, [requester]);
-		if (result.length === 0) return res.status(401).send({
-			msg: 'unauthorized'
-		});
+        let authorization = req.headers.authorization;
+        if (!authorization) {
+            return res.status(401).send({
+                msg: 'unauthorized'
+            });
+        }
+        let headers = authorization.split(';');
+        console.log(headers);
+        if (headers.length != 2 || !headers[0].startsWith('requester=') || !headers[1].startsWith('rbei_access_token=')) return res.status(401).send({
+            msg: 'unauthorized'
+        });
+        let requester = headers[0].replace('requester=', '');
+        let rbei_access_token = headers[1].replace('rbei_access_token=', '');
 
-		if (result[0].STATUS != 'A') return res.status(403).send({
-			msg: 'You are not approved'
-		});
+        //verify the token.
+        let secretKey = '$7ckugsc@#~oindjsad%9';
+        const decoded = jwt.verify(rbei_access_token, secretKey);
+        const {
+            EMAIL_ID
+        } = decoded;
+        if (!(requester === EMAIL_ID)) return res.status(401).send({
+            msg: 'unauthorized'
+        });
 
-		
 
-		req.rbei_access_role = result[0].TYPE;
-		next();
-	} catch (error) {
-		console.log(error);
-		res.status(401).send({
-			error
-		});
-	}
+        //will return only one record as email_id is the key
+        const query = 'SELECT EMAIL_ID, STATUS, TYPE FROM RBEI_CUSTOM_AUTH_T_MD_USER WHERE EMAIL_ID = ?';
+        const client = req.db;
+        const result = await client.exec(query, [requester]);
+        if (result.length === 0) return res.status(401).send({
+            msg: 'unauthorized'
+        });
+
+        if (result[0].STATUS != 'A') return res.status(403).send({
+            msg: 'You are not approved'
+        });
+
+
+
+        req.rbei_access_role = result[0].TYPE;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({
+            error
+        });
+    }
 }
 
 module.exports = auth;
--- srv/service.js
+++ srv/service.js
@@ -1,35 +1,72 @@
 const cds = require('@sap/cds')
-const jwt=require('jsonwebtoken')
-// app.listen ( process.env.PORT || 4004,()=>console.log(process.env.PORT ))
+const jwt = require('jsonwebtoken')
+
 module.exports = cds.service.impl(srv => {
-	console.log(srv.entities)
-	srv.before('*',(req)=>{
-		try{
-		let secretKey = '$7ckugsc@#~oindjsad%9'
-		// console.log(req._.req.headers.authorization)
-		let decode=jwt.verify(req._.req.headers.authorization,secretKey)
-		console.log(decode)
-		}catch(e){
-			req.error({code:'401',message:'login again'})
-		}
-	})
-	
-	srv.before('CREATE','sessions',(req)=>{
-		const session_id = 'S_' + Date.now()
-		req.data.ID = session_id
-		req.data.S_CREATED_BY = 'pratheekreddy.katta@in.bosch.com'
-		req.data.S_CREATED_ON = new Date().toISOString().replace('Z', '').replace('T', '')
-		let counter = 00
-		req.data.TOPICS.forEach(topic => {
-			topic.SUB_TOPIC_ID = 'ST_' + counter
-			topic.SESSION_ID = session_id
-			counter++
-		})
-	})
-	srv.on('READ','search',(req,next)=>{
-		// console.log(process.env.VCAP_SERVICES.objectstore[0])
-		// let {search}=srv.entities
-		// console.log(req.params)
-		// return srv.run( SELECT.from(search))
-	})
+    console.log(srv.entities)
+    srv.before('*', (req) => {
+        try {
+            let authorization = req._.req.headers.authorization;
+            if (!authorization) {
+                return req.reject({
+                    code: 401,
+                    message: 'unauthorized'
+                });
+            }
+            let headers = authorization.split(';');
+            console.log(headers);
+            if (headers.length != 2 || !headers[0].startsWith('requester=') || !headers[1].startsWith('rbei_access_token=')) return req.error({
+                code: 401,
+                message: 'unauthorized'
+            });
+            let requester = headers[0].replace('requester=', '');
+            let rbei_access_token = headers[1].replace('rbei_access_token=', '');
+
+            //verify the token.
+            let secretKey = '$7ckugsc@#~oindjsad%9';
+            const decoded = jwt.verify(rbei_access_token, secretKey);
+            const {
+                EMAIL_ID
+            } = decoded;
+            if (!(requester === EMAIL_ID)) return req.reject({
+                code: 401,
+                message: 'unauthorized'
+            });
+
+            //will return only one record as email_id is the key
+            const result = await cds.run(SELECT.from(participants, [EMAIL_ID, STATUS, TYPE]).where('EMAIL_ID=', requester))
+            if (result.length === 0) return req.reject({
+                code: 401,
+                message: 'unauthorized'
+            });
+
+            if (result[0].STATUS != 'A') return req.reject({
+                code: 401,
+                message: 'unauthorized'
+            });
+        } catch (error) {
+            return req.reject({
+                code: 401,
+                message: error
+            });
+        }
+    })
+
+    srv.before('CREATE', 'sessions', (req) => {
+        const session_id = 'S_' + Date.now()
+        req.data.ID = session_id
+        req.data.S_CREATED_BY = 'pratheekreddy.katta@in.bosch.com'
+        req.data.S_CREATED_ON = new Date().toISOString().replace('Z', '').replace('T', '')
+        let counter = 00
+        req.data.TOPICS.forEach(topic => {
+            topic.SUB_TOPIC_ID = 'ST_' + counter
+            topic.SESSION_ID = session_id
+            counter++
+        })
+    })
+    srv.on('READ', 'search', (req, next) => {
+        // console.log(process.env.VCAP_SERVICES.objectstore[0])
+        // let {search}=srv.entities
+        // console.log(req.params)
+        // return srv.run( SELECT.from(search))
+    })
 })
