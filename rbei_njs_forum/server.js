e63fc5b1b630671363ab8f6861028002dac927ae
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-09-17 07:09:59 +0000

    username validation service

--- rbei_njs_forum/server.js
+++ rbei_njs_forum/server.js
@@ -45,6 +45,7 @@ app.use('/user/auth',require("./login/login"));
 app.use('/publishagenda', require('./main/email.js'));
 app.use('/file',require('./main/fileUpload.js'));
 app.use('/admin',require('./main/userApprove'))
+app.use('/user', require('./main/validUsername'))
 	
 app.listen(port, () => {
 	console.log(`myapp is using Node.js version: ${process.version}`); 
